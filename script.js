79acab013b938ff0d5a10e8806b60f34a3b94f43
andreiextr <andreikropinov17121988@gmail.com>
2023-09-17 00:33:20 +0300

    test

---
+++ script.js
@@ -0,0 +1,5 @@
+// Получаем ссылку на элемент шапки
+var header = document.querySelector("header");
+
+// Слушаем событие прокрутки страницы и вызываем функцию fixHeader()
+window.addEventListener("scroll", header);
